7ce9e8334a272248139e5f08651d23122b2cce1b
xiang.xi <xxzzing@126.com>
2024-02-26 09:31:47 +0800

    test

--- myStudy/src/test/Test2.java
+++ myStudy/src/test/Test2.java
@@ -1,11 +1,16 @@
 package test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class Test2 {
     public static void main(String[] args) {
-        String reg=".*Element";
-        String name="Development1553Message";
-        System.out.println(Pattern.matches(reg,name));
+//        String reg=".*Element";
+////        String name="Development1553Message";
+////        System.out.println(Pattern.matches(reg,name));
+        Map<Integer,Boolean>map= new HashMap<>();
+        map.put(1,false);
+        System.out.println(map.get(1));
     }
 }
